eb777e2ede62a5e683d1e10389e1ee1edb34dfa9
Emerson Patryck <65875258+emeps@users.noreply.github.com>
2023-05-25 00:49:01 -0300

    chora: add more color variable

--- frontend/src/style/globalStyles.jsx
+++ frontend/src/style/globalStyles.jsx
@@ -6,15 +6,25 @@ export const GlobalStyle = createGlobalStyle`
         --bs-indigo: #8C52FF;
         --bs-purple:#492b7c;
         --bs-white: #fff;
+        --bs-white-100: #f5f5f5;
+        --bs-gray-100: #b3b3b3;
         --bs-gray: #868686;
         --bs-gray-600: ##777;   
         --bs-green-hover: #025802;
         --bs-green: #8eb936;    
+        --bs-purple-bold: #180133;
     }
     *{
         margin: 0;
         padding: 0;
         text-decoration: none;
+        box-sizing: border-box;
+    }
+    body{
+        background-color: var(--bs-white-100);
+        display: flex;
+        place-content: center;
+        place-items: center;
     }
 
 `
