e50bc167c2309e32cb85f88b1b3f813df8e33dd6
Emerson Patryck <65875258+emeps@users.noreply.github.com>
2023-05-25 10:43:43 -0300

    chore: remove local font CeraRoundPro add cdn font

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { GlobalStyle } from "./style/globalStyles.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { FontStyle } from "./style/fontStyle.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <GlobalStyle />
-      <FontStyle />
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- frontend/src/style/globalStyles.jsx
+++ frontend/src/style/globalStyles.jsx
@@ -2,14 +2,15 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
     :root {
-        font-family: 'Roboto', system-ui, Helvetica, Arial, sans-serif;
+        font-family: 'Cera Round Pro', system-ui, Helvetica, Arial, sans-serif;
         --bs-indigo: #8C52FF;
         --bs-purple:#492b7c;
         --bs-white: #fff;
         --bs-white-100: #f5f5f5;
         --bs-gray-100: #b3b3b3;
         --bs-gray: #868686;
-        --bs-gray-600: ##777;   
+        --bs-gray-600: ##777; 
+        --bs-gray-700: #4e5052;  
         --bs-green-hover: #025802;
         --bs-green: #8eb936;    
         --bs-purple-bold: #180133;
@@ -19,6 +20,7 @@ export const GlobalStyle = createGlobalStyle`
         padding: 0;
         text-decoration: none;
         box-sizing: border-box;
+        
     }
     body{
         background-color: var(--bs-white-100);
