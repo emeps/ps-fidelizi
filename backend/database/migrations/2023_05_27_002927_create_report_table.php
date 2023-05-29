368c2448c75037aceadd1d5637d0bd960ef19d48
Emerson Patryck <65875258+emeps@users.noreply.github.com>
2023-05-29 20:46:56 -0300

    feat: add controller, model, factory and migration Report

---
+++ backend/database/migrations/2023_05_27_002927_create_report_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('report', function (Blueprint $table) {
+            $table->id();
+            $table->integer('offer_id');
+            $table->integer('amount_remaining');
+            $table->integer('amount_actived');
+            $table->boolean('offer_active');
+            $table->dateTime('date_executed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('report');
+    }
+};
