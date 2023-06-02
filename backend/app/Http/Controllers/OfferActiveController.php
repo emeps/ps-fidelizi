f2bfd69c03dd6c89ac0cf02a096b40f1a30ce6eb
Emerson Patryck <65875258+emeps@users.noreply.github.com>
2023-06-02 12:27:43 -0300

    feat: add OfferActiveController to manage information sent to front

---
+++ backend/app/Http/Controllers/OfferActiveController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Offer;
+use App\Models\Reward;
+use Illuminate\Http\Request;
+
+class OfferActiveController extends Controller
+{
+     
+    // public function __construct(
+    //     private readonly  Offer $offer,
+    //     private readonly  Reward $reward,
+    // ){}
+    
+}
