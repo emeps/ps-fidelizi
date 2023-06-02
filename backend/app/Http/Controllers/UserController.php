08f3ea4a0b0c87c3742a116fe3c7d94a0d2ccd2f
Emerson Patryck <65875258+emeps@users.noreply.github.com>
2023-06-02 12:18:50 -0300

    feat: add CRUD user

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Models\User;
+use App\Models\Offer;
+use App\Notifications\OfferNotification;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+use function App\Helpers\only_numbers;
+use function App\Helpers\validate_cpf;
+
+class UserController extends Controller
+{
+    public function __construct(
+        private readonly User $user, 
+        private readonly Offer $offer
+        
+        )
+    {
+    }
+    public function index(): JsonResponse
+    {
+        try {
+            $users = $this->user->all();
+            return response()->json($users, Response::HTTP_OK);
+        } catch (\Exception $e) {
+            return response()->json(['data' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request): JsonResponse
+    {
+        try {
+            $data = $request->validated();
+            $user = $this->user->create($data);
+            return response()->json($user, Response::HTTP_CREATED);
+        } catch (\Exception $e) {
+            return response()->json(['data' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function show(int $userId): JsonResponse
+    {
+        $user = $this->user->newQuery()->findOrFail($userId);
+        return response()->json($user, Response::HTTP_OK);
+    }
+
+    // TODO: Verificar o codigo de resposta para cada função
+
+    public function update(UpdateUserRequest $request, int $userId): JsonResponse
+    {
+        $data = $request->validated();
+        $user = $this->user->newQuery()->findOrFail($userId);
+        try {
+            $user->update($data);
+            return response()->json($user, Response::HTTP_OK);
+        } catch (\Exception $e) {
+            return response()->json(['data' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function destroy(int $userId): JsonResponse
+    {
+        $user = $this->user->newQuery()->findOrFail($userId);
+        return response()->json($user->delete(), Response::HTTP_NO_CONTENT);
+    }
+
+    public function checkUser(Request $request, int $offerId): JsonResponse
+    {
+        if (!validate_cpf($request->cpf)) {
+            return response()->json(['data' => 'CPF inválido'], Response::HTTP_BAD_REQUEST);
+        } else {
+            $cpf = only_numbers($request->cpf);
+
+            $user = $this->user->newQuery()->where('cpf', $cpf)->firstOrFail();
+
+            /** @var Offer */
+            
+            $offer = $this->offer->newQuery()->findOrFail($offerId);
+
+            $user->offers()->toggle($offer->id);
+
+            //Enviar email
+            $user->notify(new OfferNotification($offer));
+            return response()->json(['data'=>'Promoção cadastrada'], Response::HTTP_OK);
+        }
+    }
+
+}
---
+++ backend/app/Http/Requests/StoreUserRequest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'fullname' => 'required|string|max:255',
+            'email'=> 'required|string|email|max:255|unique:users',
+            'cpf'=> 'required|string|max:11|unique:users',
+            'date_birth'=> 'required|date_format:d-m-Y|date',
+            'phone'=> 'required|string|max:11',
+        ];
+    }
+    
+    public function messages(): array
+    {
+        return [
+            'fullname.required' => 'O campo nome é obrigatório',
+            'fullname.string' => 'O campo nome deve ser uma string',
+            'fullname.max' => 'O campo nome deve ter no máximo 255 caracteres',
+            'email.required' => 'O campo email é obrigatório',
+            'email.string' => 'O campo email deve ser uma string',
+            'email.email' => 'O campo email deve ser um email válido',
+            'email.max' => 'O campo email deve ter no máximo 255 caracteres',
+            'email.unique' => 'O campo email já está em uso',
+            'cpf.required' => 'O campo cpf é obrigatório',
+            'cpf.string' => 'O campo cpf deve ser uma string',
+            'cpf.max' => 'O campo cpf deve ter no máximo 11 caracteres',
+            'cpf.unique' => 'O campo cpf já está em uso',
+            'date_birth.required' => 'O campo data de nascimento é obrigatório',
+            'date_birth.date_format' => 'O campo data de nascimento deve ser uma data válida',
+            'date_birth.date' => 'O campo data de nascimento deve ser uma data válida',
+            'phone.required' => 'O campo telefone é obrigatório',
+            'phone.string' => 'O campo telefone deve ser uma string',
+            'phone.max' => 'O campo telefone deve ter no máximo 11 caracteres',
+        ];
+    }
+
+}
