0a38203e06377bd206051da3e22a83af816d144b
Emerson Patryck <65875258+emeps@users.noreply.github.com>
2023-06-02 22:27:20 -0300

    chore: add new fields to model

--- backend/app/Models/Report.php
+++ backend/app/Models/Report.php
@@ -8,9 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Report extends Model
 {
     use HasFactory;
+    public $table = 'report';
     protected $fillable = [
+        'enterprise_name',
         'description',
-        'customer_id',
-        'offer_id',
+        'reward',
+        'amount_remaining',
+        'amount_actived',
+        'offer_active',
+        'date_created',
+        'date_remaining',
+        'date_executed',
     ];
 }
