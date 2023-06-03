da2b858d9c02337bbcee79df30a753d426ece479
Emerson Patryck <65875258+emeps@users.noreply.github.com>
2023-06-02 22:37:47 -0300

    feat: add report controller

--- backend/app/Http/Controllers/ReportController.php
+++ backend/app/Http/Controllers/ReportController.php
@@ -5,62 +5,36 @@ namespace App\Http\Controllers;
 use App\Models\Report;
 use App\Http\Requests\StoreReportRequest;
 use App\Http\Requests\UpdateReportRequest;
+use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    public function __construct(
+        private readonly Report $report
+    ) {
+    }
+
     public function index()
     {
-        //
+        return $this->report->all();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function store(Request $request)
     {
-        //
+        $data = $request->validated();
+        return $this->report->create($data);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreReportRequest $request)
+    public function show(int $reportId)
     {
-        //
+        return $this->report->newQuery()->findOrFail($reportId);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Report $report)
+    public function update(Request $request, int $reportId)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Report $report)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateReportRequest $request, Report $report)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Report $report)
-    {
-        //
+        $data = $request->validated();
+        $report = $this->report->newQuery()->findOrFail($reportId);
+        $report->update($data);
+        return $report;
     }
 }
