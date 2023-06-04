5090517479e91490a476d55dc91b4b2f9c186351
Emerson Patryck <65875258+emeps@users.noreply.github.com>
2023-06-04 12:18:45 -0300

    feat: add Title component

--- frontend/src/components/Title/index.jsx
+++ frontend/src/components/Title/index.jsx
@@ -1,13 +1,23 @@
 import { SubtitleSpan, TitleContainer, TitleH1 } from "./Title.style";
+import { api } from "../../api/axios.api";
+import { useEffect,useState } from "react";
 
-import { dataMock } from "../../mock/data";
 
 export const Title = () => {
+  const [title, setTitle] = useState({})
+  const getTitle = async () => {
+    const response = await api.get(`/offers/1`)
+    setTitle(response.data.offer)
+  }
+  useEffect(() => {
+    getTitle()
+  },[])
+
   return (
     <>
       <TitleContainer>
         <SubtitleSpan>Demonstração FideliZi</SubtitleSpan>
-        <TitleH1>{dataMock.description}</TitleH1>
+        <TitleH1>{title.reward + ' ' + title.description}</TitleH1>
       </TitleContainer>
     </>
   );
