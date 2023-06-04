003f83fbe49d7be3090136201af58179f27e8fb4
Emerson Patryck <65875258+emeps@users.noreply.github.com>
2023-06-04 12:20:25 -0300

    feat: add Offer component and business logic

--- frontend/src/components/Offer/Offer.style.js
+++ frontend/src/components/Offer/Offer.style.js
@@ -5,7 +5,8 @@ export const OfferContainer = styled.section`
     width: 100%;
     align-items: flex-start;
     justify-content: center;
-` 
+    padding-left: 20px;
+`
 export const OfferImage = styled.img`  
     max-width: 320px;
     object-position: center;
@@ -16,7 +17,6 @@ export const OfferContainerImage = styled.div`
     min-width: 300px;
     border-radius: 10px;
     overflow: hidden;
-    padding-left: 20px;
 `
 export const OfferContainerComponent = styled.div`
     display: flex;
@@ -37,11 +37,31 @@ export const OfferTextFooter = styled.pre`
     line-height: 18px;
     margin-bottom: 1rem;
     overflow: auto;
+    font-family: 'Cera Round Pro', system-ui, Helvetica, Arial, sans-serif;
+
+    
+`
+export const OfferShared = styled.span`
 `
 
-
 export const OfferActiveContainer = styled.div`
-
+    width: 100%;
+    display: -webkit-flex;
+    display: flex;
+    -webkit-align-items: center;
+    align-items: center;
+    -webkit-justify-content: center;
+    justify-content: center;
+    position: relative;
+    background-color: #8eb936;
+    font-weight: 700;
+    padding: 19px 105px 17px 17px;
+    border-radius: 7px;
+    color: var(--bs-white);
+    &:hover{
+        background-color: #025802;
+        cursor: pointer;
+    }
 `
 export const OfferActiveButton = styled.button`
     background-color: var(--bs-green);
@@ -50,14 +70,52 @@ export const OfferActiveButton = styled.button`
     }
     
 `
+export const OfferAviableQuantity = styled.span`
+    font-size: 18px;
+`
+
+export const OfferAviableQuantitySpan = styled.span`
+    font-size: 11px;
+    font-weight: 400;
+`
+
+export const OfferActive = styled.span`
+    font-size: 14px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 12px;
+`
+export const OfferAviable = styled.div`
+    background-color: #025802;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    position: absolute;
+    text-align: center;
+    line-height: 10px;
+    padding-right: 12px;
+    padding-top: 3px;
+    padding-left: 20px;
+    height: 100%;
+    margin-top: -2px;
+    border-radius: 0 7px 7px 0;
+    right: 0;
+    top: 2px;
+`
 export const OfferContainerQuantity = styled.div`
 
 `
 export const OfferSharedContainer = styled.div`
-    width: 40%;
+    width: 100%;
 `
-export const OfferSharedSpan = styled.span`
-
+export const OfferSharedSpan = styled.h4`
+    font-size: 20px;
+    text-align: center;
+    width: 100%;
+    color: #492b7c;
+    margin-bottom: 0px;
 `
 
 export const OfferSharedButton = styled.div`
@@ -71,6 +129,7 @@ export const OfferSharedButton = styled.div`
     padding: 7px;
     margin-top: 6px;
     border-radius: 7px;
+    gap: 10px;
     &:hover{
         background-color: #666;
         color: #fff;
@@ -102,7 +161,9 @@ export const OfferAlertBody = styled.span`
     justify-content: center;
     padding: 5px;
     font-size: 15px;
+    gap: 8px;
     svg{
+        width: 14px;
         font-size: 21px;
     }
 `
--- frontend/src/components/Offer/index.jsx
+++ frontend/src/components/Offer/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
 import {
   OfferContainer,
@@ -6,89 +7,326 @@ import {
   OfferContainerComponent,
   OfferTextFooter,
   OfferActiveContainer,
-  // OfferActiveButton,
   OfferSharedSpan,
   OfferSharedContainer,
   OfferAlertContainer,
   OfferAlertHeader,
   OfferAlertBody,
   OfferSharedButton,
-  // OfferContainerQuantity
+  OfferShared,
+  OfferAviable,
+  OfferActive,
+  OfferAviableQuantity,
+  OfferAviableQuantitySpan,
 } from "./Offer.style";
-import { dataMock } from "../../mock/data";
+import { SocialMedia } from "../SocialMedia";
+
+import { Button } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClock } from "@fortawesome/free-solid-svg-icons";
-// import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { faShare } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
-// import Modal from "react-modal";
-
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { Form, Modal, InputGroup } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { IMaskInput } from "react-imask";
+import { Br } from "react-flags-select";
 import { ModalFidelizi } from "../Modal";
 
-// TODO: Arrumar o raio da borda da imagem (OfferContainerImage)
-// TODO: Estilizar Botão dos modais
-// TODO: Estilizar os modais
-// TODO: Estilizar a tag pre
-// TODO: Inserir o campo e o botão de envio
+import { api } from "../../api/axios.api";
+import validatedCPF from '../../helpers/validateCPF'
+
 // TODO: Inserir o loading
 // TODO: Inserir a mensagem de sucesso e erro
 
 export const Offer = () => {
+  // Controle do Modal
   const [modalShowActive, setModalShowActive] = useState(false);
   const [modalShowShared, setModalShowShared] = useState(false);
+
+  // Controler do conteudo da página
+  const [offer, setOffer] = useState({});
+
+  // Controler do CPF
+  const [cpf, setCpf] = useState("");
+
+  // Controle dos cadastro
+  const[fullname, setFullname] = useState('');
+  const[email, setEmail] = useState('');
+  const[phone, setPhone] = useState('');
+  const[dataBirth, setDataBirth] = useState('');
+
+  //Contole das requisições
+  const [isCreateUser, setIsCreateUser] = useState(false);
+
+  // Função para pegar a oferta
+  const getOffer = async () => {
+    const response = await api.get("/offers/1");
+    setOffer(response.data.offer);
+  };
+
+  const handleCPF = (e) => {
+    setCpf(e.target.value);
+  };
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await api.post("/check-user/1", { cpf });
+    constructorModal(data.offer_code);
+    
+  };
+  const createUser = async (e) => {
+    e.preventDefault();
+    const { data } = await api.post("/check-create-user/1", { cpf, fullname, email, phone, dataBirth });
+  }
+  const constructorModal = (offer_code) => {
+    switch (offer_code) {
+      case "OFC100":
+        console.log("CPF inválido");
+        return;
+      case "OFC101":
+        console.log("Usuário não encontrado");
+        setIsCreateUser(true)
+        return;
+
+      case "OFC102":
+        console.log("Oferta esgotada");
+        return;
+
+      case "OFC103":
+        console.log("Usuário já possui essa oferta");
+        return;
+
+      case "OFC200":
+        console.log("Oferta ativada com sucesso");
+        return;
+
+      default:
+        console.log("Eu nçao li nada ");
+        return;
+    }
+  };
+
+  // Função para limpar os campos
+  const clearInputs = () => {
+    setCpf("");
+  };
+
+  useEffect(() => {
+    getOffer();
+  }, [isCreateUser]);
 
   return (
     <>
       <OfferContainer>
         <OfferContainerImage>
-          <OfferImage src={dataMock.image} />
+          <OfferImage src={offer.image} />
         </OfferContainerImage>
         <OfferContainerComponent>
           <OfferActiveContainer>
-            {/* <OfferActiveButton onClick={() => setModalShowActive(true)}>
-              <span>
-                <FontAwesomeIcon icon={faCheck} /> ATIVAR OFERTA
-              </span>
-              <OfferContainerQuantity>
-                <span>{dataMock.quantity}</span>
+            <div onClick={() => setModalShowActive(true)}>
+              <OfferActive>
+                <FontAwesomeIcon icon={faCheck} /> <span>ATIVAR OFERTA</span>
+              </OfferActive>
+              <OfferAviable>
+                <OfferAviableQuantity>
+                  {offer.amount_registered}
+                </OfferAviableQuantity>
                 <br />
-                <span>{dataMock.complementQuantity}</span>
-              </OfferContainerQuantity>
-            </OfferActiveButton> */}
+                <OfferAviableQuantitySpan>disponíveis</OfferAviableQuantitySpan>
+              </OfferAviable>
+            </div>
           </OfferActiveContainer>
           <OfferSharedContainer>
-            <OfferSharedButton  onClick={() => setModalShowShared(true)} >
-              <OfferSharedSpan>
-                <FontAwesomeIcon icon={faShare} /> COMPARTILHAR
-              </OfferSharedSpan>
+            <OfferSharedButton onClick={() => setModalShowShared(true)}>
+              <FontAwesomeIcon icon={faShare} />
+              <OfferShared>COMPARTILHAR</OfferShared>
             </OfferSharedButton>
           </OfferSharedContainer>
           <OfferAlertContainer>
-            <OfferAlertHeader>{dataMock.alertDescription}</OfferAlertHeader>
+            <OfferAlertHeader>
+              OFERTA DISPONÍVEL POR TEMPO LIMITADO! FALTAM:
+            </OfferAlertHeader>
             <OfferAlertBody>
-              <FontAwesomeIcon icon={faClock} /> <span>{dataMock.days}</span>
+              <FontAwesomeIcon icon={faClock} />{" "}
+              <span>{offer.date_remaing + " dias restantes"}</span>
             </OfferAlertBody>
           </OfferAlertContainer>
         </OfferContainerComponent>
       </OfferContainer>
-      <OfferTextFooter>{dataMock.description}</OfferTextFooter>
+      <OfferTextFooter>
+        {offer.reward + " " + offer.description}
+      </OfferTextFooter>
 
       <ModalFidelizi
         show={modalShowActive}
-        onHide={() => setModalShowActive(false)}
-        title={"ATIVAR OFERTA"}
+        onHide={() => {
+          setModalShowActive(false);
+          clearInputs();
+        }}
+        title={"ATIVE SUA OFERTA"}
       >
-        <div>Colocar aqui input para CPF</div>
+        {!isCreateUser ? (
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-2 text-center">
+              <Form.Label>
+                Para ativar a oferta, digite seu CPF para identificação.
+              </Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Insira seu CPF"
+                as={IMaskInput}
+                mask="000.000.000-00"
+                className="text-center mt-2 font-weight-bolder"
+                onChange={handleCPF}
+              />
+
+              <Button
+                disabled={!validatedCPF(cpf)}
+                className="text-center mt-2  px-5"
+                type="submit"
+                style={{ backgroundColor: "#834be1" }}
+              >
+                Continuar <FontAwesomeIcon icon={faAngleRight} />
+              </Button>
+            </Form.Group>
+          </Form>
+
+
+        ) : (
+          <Form className="mb-2 text-center">
+            <Form.Group className="mb-2 text-center">
+              <Form.Label>
+                <strong>
+                  Para ativar esta oferta, é necessário participar do Fidelizi
+                  em{" "}
+                  <a
+                    href="https://fidelizii.com.br/demonstracao-fidelizi"
+                    style={{ color: "#834be1" }}
+                  >
+                    Demostração FideliZi!
+                  </a>
+                </strong>
+              </Form.Label>
+              <Form.Text className="text-muted">
+                Preencha os campos abaixo para participar!
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3 border border-secondary p-4 border-opacity-25 rounded-4 text-start">
+              <Form.Label className="mb-3">
+                <strong>Dados Essenciais</strong>
+              </Form.Label>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>
+                  Nome completo <Form.Text muted>(Obritagório)</Form.Text>
+                </Form.Label>
+                <Form.Control type="text" />
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Label>
+                  Email <Form.Text muted>(Obritagório)</Form.Text>
+                </Form.Label>
+                <Form.Control type="email" />
+              </Form.Group>
+            </Form.Group>
+            <Form.Group className="mb-3 border border-secondary p-4 border-opacity-25 rounded-4 text-start">
+              <Form.Label className="mb-3">
+                <strong>Dados Adicionais</strong>
+              </Form.Label>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>
+                  Data de Nascimento <Form.Text muted>(Obritagório)</Form.Text>
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  as={IMaskInput}
+                  mask="00/00/0000"
+                  placeholder="DD/MM/AAAA"
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <InputGroup className="mb-3">
+                  <InputGroup.Text id="basic-addon1">
+                    <Br />
+                  </InputGroup.Text>
+                  <Form.Control
+                    type="text"
+                    as={IMaskInput}
+                    mask="+55 (00) 00000-0000"
+                    placeholder="+55"
+                  />
+                </InputGroup>
+              </Form.Group>
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Button
+                className="text-center mt-2  px-5"
+                type="submit"
+                style={{ backgroundColor: "#834be1" }}
+              >
+                Continuar <FontAwesomeIcon icon={faAngleRight} />
+              </Button>
+              <Form.Group className="mb-2 text-center">
+                <Form.Label className= "px-2 mt-3">
+                  <span>
+                    Ao participar você concorda e aceita os{" "}
+                    <a
+                      href="https://fidelizii.com.br/termos-de-uso"
+                      style={{ color: "#834be1" }}
+                    >
+                      termos de uso
+                    </a>{" "}
+                    e{" "}
+                    <a
+                      href="https://fidelizii.com.br/politica-de-privacidade-e-cookies"
+                      style={{ color: "#834be1" }}
+                    >
+                      políticas de privacidade
+                    </a>{" "}
+                    do FideliZi{" "}
+                  </span>
+                </Form.Label>
+              </Form.Group>
+            </Form.Group>
+          </Form>
+        )}
       </ModalFidelizi>
-      <ModalFidelizi
+
+      {/* TODO:Refatorar o modal para reaproveitar como componente */}
+
+      <Modal
         show={modalShowShared}
-        onHide={() => setModalShowShared(false)}
-        title={"COMPARTILHAR"}
+        onHide={() => {
+          setModalShowShared(false);
+        }}
       >
-        <div>
-          Colocar aqui o component com as imagens e links para compartilhar
-        </div>
-      </ModalFidelizi>
+        <Modal.Header
+          closeButton
+          style={{
+            border: "none",
+            borderBottom: "1px dashed #dee2e6",
+          }}
+        >
+          <Modal.Title
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: "100%",
+            }}
+          >
+            <OfferSharedSpan>COMPARTILHE ESSA OFERTA</OfferSharedSpan>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {" "}
+          <SocialMedia />{" "}
+        </Modal.Body>
+      </Modal>
     </>
   );
 };
