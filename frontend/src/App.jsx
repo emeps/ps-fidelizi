5ed97c05bc58dce4f29e6c2a7a99af932776f79b
Emerson Patryck <65875258+emeps@users.noreply.github.com>
2023-06-04 12:18:21 -0300

    chore: rework App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,9 @@
-import { Board } from "./components/Board";
-import { Footer } from "./components/Footer";
-import { Header } from "./components/Header";
-import { Instructions } from "./components/Instructions";
-import { Offer } from "./components/Offer";
-import { Rules } from "./components/Rules";
-import { Separator } from "./components/Separator";
-import { Social } from "./components/Social";
-import { Title } from "./components/Title";
+import OfferPage from "./routes/OfferPage";
 
-import { Wrapper } from "./components/Wrapper";
 
-// TODO: Alterar div para section para manter a semantica
 function App() {
   return (
-    <>
-      <Board>
-        <Wrapper>
-          <Header />
-          <Title />
-          <Offer />
-          <Separator />
-          <Instructions />
-          <Separator />
-          <Rules />
-          <Social />
-        </Wrapper>
-      </Board>
-      <Footer />
-    </>
+   <OfferPage/>
   );
 }
 
